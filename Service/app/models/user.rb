0d9a2186d0886be188f8d03af0ebf8700435272c
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-13 14:50:23 +0800

    feat: added User and Portfolio association and migrated changes

--- Service/app/models/user.rb
+++ Service/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :portfolios, dependent: :destroy
+
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable,
