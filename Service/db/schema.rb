94ed055d7938cddae5fbeea7a5fd0475875782d7
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-13 15:16:51 +0800

    feat: add Jti col to Users for token revocation and migrated

--- Service/db/schema.rb
+++ Service/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_13_065443) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_071505) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,8 +38,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_13_065443) do
     t.string "last_name", null: false
     t.string "role", null: false
     t.string "signup_status", default: "pending", null: false
+    t.string "jti", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
