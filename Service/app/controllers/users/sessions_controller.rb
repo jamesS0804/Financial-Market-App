6e2c13fedf24f616bab2600c6e7f7602d9b6320b
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-13 19:11:55 +0800

    wip: pending sessions_controller test; pending sessions_controller action logic

--- Service/app/controllers/users/sessions_controller.rb
+++ Service/app/controllers/users/sessions_controller.rb
@@ -1,28 +1,47 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(resource, _opts={})
+    expiry_object = Time.parse("#{Time.now + 30.minutes.to_i}")
+    expiry_in_seconds = expiry_object.to_i
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    response.headers['expiry'] = expiry_in_seconds
+    render json: {
+      status: {
+        code: 200,
+        message: 'Logged in successfully'
+      },
+      data: {
+        user: UserSerializer.new(resource).serializable_hash[:data][:attributes]
+      }
+    }, status: :ok
+  end
 
-  # protected
+  def respond_to_on_destroy
+    begin
+      jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], ENV['DEVISE_JWT_SECRET_KEY'], true, algorithm: 'HS256', verify_jti: true)
+      user_id = jwt_payload[0]['sub']
+      current_user = User.find(user_id)
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+      if current_user
+        sign_out(current_user)
+
+        render json: {
+          status: 200,
+          message: "logged out successfully"
+        }, status: :ok
+      end
+    rescue JWT::DecodeError
+      render json: {
+        status: 401,
+        message: "Invalid authentication token."
+      }, status: :unauthorized
+    end
+  end
+
+  def sessions_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :first_name, :last_name, :role)
+  end
 end
