794bc5ca019ca133f91fadcd1acf208a51166e42
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-13 15:08:38 +0800

    chore: setup shoulda-matchers gem; added User tests

--- Service/spec/models/user_spec.rb
+++ Service/spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should have_many(:portfolios).dependent(:destroy) }
+  end
+
+  describe "devise configuration" do
+    it "includes Devise JWT Revocation Strategies JTIMatcher" do
+      expect(User.included_modules).to include(Devise::JWT::RevocationStrategies::JTIMatcher)
+    end
+
+    it "is database authenticatable" do
+      expect(User.devise_modules).to include(:database_authenticatable)
+    end
+
+    it "is registerable" do
+      expect(User.devise_modules).to include(:registerable)
+    end
+
+    it "is validatable" do
+      expect(User.devise_modules).to include(:validatable)
+    end
+
+    it "is confirmable" do
+      expect(User.devise_modules).to include(:confirmable)
+    end
+
+    it "is JWT authenticatable" do
+      expect(User.devise_modules).to include(:jwt_authenticatable)
+    end
+  end
 end
