ad94a50711eb69b821766f74aa315aabe855df14
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-13 15:08:38 +0800

    chore: setup shoulda-matchers gem; added User tests

--- Service/app/models/user.rb
+++ Service/app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable, :confirmable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 end
