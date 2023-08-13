4cd38480473ce39c930a4042de1f870777a32302
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-13 22:59:21 +0800

    fix: updated User table role to integer datatype to match User Model role as enum; changed registrations_controller create logic to implement sign_up_params for better security

--- Service/db/migrate/20230813065443_add_informational_columns_to_users.rb
+++ Service/db/migrate/20230813065443_add_informational_columns_to_users.rb
@@ -2,7 +2,7 @@ class AddInformationalColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :first_name, :string, null: false
     add_column :users, :last_name, :string, null: false
-    add_column :users, :role, :string, null: false
+    add_column :users, :role, :integer, null: false, default: 0
     add_column :users, :signup_status, :string, null: false, default: "pending"
   end
 end
