373fbfc9fffe2cc7373438406a28afd0c42085f3
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-18 13:11:27 +0800

    chore: in admins_controller_spec, added 'GET /view_trader' test

--- Service/spec/requests/admins_controller_spec.rb
+++ Service/spec/requests/admins_controller_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe "Admins", type: :request do
   describe "POST /create_trader" do
     context "when an admin successfully creates a new trader" do
       it "returns a JSON response with a success message and created trader's information" do
-        post "/admin/create_trader", params: { user: { id: 1, email: 'user@example.com', password: 'password', first_name: 'test', last_name: 'test', role: "trader" } }
+        post "/admin/create_trader", params: { user: { id: 1, email: 'user1@example.com', password: 'password', first_name: 'test', last_name: 'test', role: "trader" } }
 
         expect(response).to have_http_status(:ok)
 
         json_response = JSON.parse(response.body)
 
-        expect(json_response['data']['email']).to eq("user@example.com")
+        expect(json_response['data']['email']).to eq("user1@example.com")
         expect(json_response['data']['password']).to eq("password")
         expect(json_response['data']['first_name']).to eq("test")
         expect(json_response['data']['last_name']).to eq("test")
@@ -37,12 +37,31 @@ RSpec.describe "Admins", type: :request do
 
         json_response = JSON.parse(response.body)
 
-        puts json_response
-
         expect(json_response['data']['email']).to eq("updated_email@example.com")
         expect(json_response['data']['password']).to eq("updated_password")
         expect(json_response['data']['first_name']).to eq("updated_first_name")
         expect(json_response['data']['last_name']).to eq("updated_last_name")
+      end
+    end
+  end
+
+  describe "GET /view_trader" do
+    context "when an admin wants to view a trader's info" do
+      it "returns a JSON response with a success message and trader's information" do
+        get "/admin/view_trader/#{user.id}"
+
+        expect(response).to have_http_status(:ok)
+
+        json_response = JSON.parse(response.body)
+
+        puts json_response
+
+        expect(json_response['data']['email']).to eq("user@example.com")
+        expect(json_response['data']['first_name']).to eq("test")
+        expect(json_response['data']['last_name']).to eq("test")
+        expect(json_response['data']['created_at']).to_not be_nil
+        expect(json_response['data']['confirmed_at']).to eq(nil)
+        expect(json_response['data']['signup_status']).to eq("pending")
       end
     end
   end
