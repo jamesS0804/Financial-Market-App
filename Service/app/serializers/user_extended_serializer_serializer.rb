100354a93484a001fb4fd8e695ab1f3e2be36418
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-18 11:47:52 +0800

    feat: added user_extended_serializer to include necessary data for admin actions; implemented said serializer to admins_controller create_trader

---
+++ Service/app/serializers/user_extended_serializer_serializer.rb
@@ -0,0 +1,4 @@
+class UserExtendedSerializerSerializer
+  include JSONAPI::Serializer
+  attributes :id, :email, :password, :first_name, :last_name, :role, :created_at, :confirmed_at, :signup_status
+end
