c0852f34c29cadc2e925da743782ef2e02503e7e
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-18 16:48:38 +0800

    chore: in portfolio_spec, added associations tests: have_many and belongs_to; also added validations tests: presence and uniqueness

--- Service/spec/models/portfolio_spec.rb
+++ Service/spec/models/portfolio_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Portfolio, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { should have_many(:transactions).dependent(:destroy) }
+    it { should belong_to(:user) }
+  end
+
+  describe "validations" do
+    portfolio_params = { name: "Test Portfolio" }
+
+    it "should not be valid without name" do
+      portfolio = Portfolio.create(portfolio_params)
+      portfolio[:name] = nil
+      expect(portfolio).to_not be_valid
+    end
+    it "should not be valid if it's not unique" do
+      original_portfolio = Portfolio.create(portfolio_params)
+      duplicate_portfolio = Portfolio.create(portfolio_params)
+      expect(duplicate_portfolio).to_not be_valid
+    end
+  end
 end
