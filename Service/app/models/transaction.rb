10116a4780a1b59748c3c74aa03044bf0692bb9c
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-24 06:30:35 +0800

    feat: added foreign key in Transactionand Market for has_many relationship

--- Service/app/models/transaction.rb
+++ Service/app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
     belongs_to :portfolio
-    belongs_to :market
+    belongs_to :market, foreign_key: 'market_name', primary_key: 'name'
 
     enum transaction_type: [:BUY, :SELL]
 end
