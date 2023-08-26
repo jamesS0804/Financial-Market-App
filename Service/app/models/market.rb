aa11e1c20fbaccd8be1c271207967c8955fd1cdc
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-27 07:30:51 +0800

    fix: added primary_key to Transaction model for proper reference to Market model

--- Service/app/models/market.rb
+++ Service/app/models/market.rb
@@ -1,6 +1,6 @@
 class Market < ApplicationRecord
     enum name: [ :STOCK, :FOREX, :CRYPTO ]
 
-    has_many :transactions, foreign_key: 'market_name'
+    has_many :transactions, foreign_key: 'market_name', primary_key: 'name'
     has_many :units, foreign_key: 'market_name'
 end
--- Service/app/models/transaction.rb
+++ Service/app/models/transaction.rb
@@ -2,5 +2,5 @@ class Transaction < ApplicationRecord
     belongs_to :portfolio
     belongs_to :market, foreign_key: 'market_name', primary_key: 'name'
 
-    enum transaction_type: [:BUY, :SELL]
+    enum transaction_type: [ :BUY, :SELL ]
 end
