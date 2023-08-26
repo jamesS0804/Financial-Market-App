659250cf27fd2a2d0ed78130c1e67561b36b2f8f
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-27 07:29:16 +0800

    chore: added test for 'POST /transactions#process_market_order_buy'

---
+++ Service/spec/requests/transactions_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe "TransactionsController", type: :request do
+  describe "POST /process_limit_order_buy" do
+
+  end
+
+  describe "POST /process_limit_order_sell" do
+
+  end
+
+  describe "POST /process_market_order_buy" do
+    context "when a trader wants to buy stock market order" do
+      it "should send a JSON response with a success message and transaction receipt" do
+        user = User.create(email: 'user1@example.com', password: 'password', first_name: 'test', last_name: 'test', role: "TRADER", confirmed_at: Date.today)
+        portfolio = user.portfolios.create(name: 'test portfolio')
+
+        post "/portfolios/#{portfolio.id}/transactions/market_order/buy", params: { 
+          transaction: {
+              market_name: 'STOCK',
+              market_order_type: 'MARKET',
+              transaction_type: 'BUY',
+              status: 'FILLED',
+              symbol: 'AAPL',
+              quantity: 100,
+              price: 150.00
+            } 
+        }
+        
+        puts JSON.parse(response.body).inspect
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "POST /process_market_order_sell" do
+
+  end
+end
