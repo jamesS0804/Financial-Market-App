a2197b372a0c298758f3032fd7ef0db253cc6e60
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-28 07:32:28 +0800

    feat: Initialized routes and pages
    -Installed and setup react-browser-react
    -Created the following pages and linked their routes:
            HomePage
            LoginPage
            SignupPage
            DashboardPage

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from './pages/SignupPage.jsx';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+        <Route index path="/" element={<HomePage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/signup" element={<SignupPage />}/>
+        <Route path="/dashboard" elemet={<DashboardPage />}/>
+    </Routes>
   )
 }
 
