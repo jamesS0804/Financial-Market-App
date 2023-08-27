c770e93a36132fd8961e0ac9a63b13948d581779
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-27 09:22:16 +0800

    feat: added stock data columns to Portfolios table; added validations for settled_cash and buying_power to not be less than 0

--- Service/app/models/portfolio.rb
+++ Service/app/models/portfolio.rb
@@ -4,4 +4,6 @@ class Portfolio < ApplicationRecord
     has_many :transactions, dependent: :destroy
 
     validates :name, presence: true, uniqueness: { scope: :user_id }
+    validates :settled_cash, numericality: { greater_than_or_equal_to: 0, message: 'must be greater than or equal to 0' }
+    validates :buying_power, numericality: { greater_than_or_equal_to: 0, message: 'must be greater than or equal to 0' }
 end
