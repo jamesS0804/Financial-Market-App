b9d730e8e3a1dde3b2a65884f7e6884af226aea2
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-27 10:07:18 +0800

    chore: added test for POST/transactions#process_limit_order_buy; added test for said action's invalidity as well

--- Service/spec/requests/transactions_controller_spec.rb
+++ Service/spec/requests/transactions_controller_spec.rb
@@ -5,91 +5,139 @@ RSpec.describe "TransactionsController", type: :request do
   before(:each) do
     market = Market.create(name: 'STOCK')
     @portfolio = user.portfolios.create(name: 'test portfolio', settled_cash: 1000.00, buying_power: 1000.00, market_value: 500.00)
-    @transaction_params = { 
-      transaction: {
-        market_name: 'STOCK',
-        market_order_type: 'MARKET',
-        transaction_type: 'BUY',
-        status: 'FILLED',
-        symbol: 'AAPL',
-        quantity: 100,
-        price: 150.00
+  end
+  
+  describe "Stock Market orders" do
+    before(:each) do
+      @transaction_params = { 
+        transaction: {
+          market_name: 'STOCK',
+          market_order_type: 'MARKET',
+          transaction_type: 'BUY',
+          status: 'FILLED',
+          symbol: 'AAPL',
+          quantity: 100,
+          price: 150.00
+        }
       }
-    }
-  end
-
-  describe "POST /process_limit_order_buy" do
-
-  end
-
-  describe "POST /process_limit_order_sell" do
-
-  end
-
-  describe "POST /process_market_order_buy" do
-    context "when a trader successfully buys stock market order" do
-      it "should send a JSON response with a success message and transaction receipt and updated profile" do
-        post "/portfolios/#{@portfolio.id}/transactions/market_order/buy", params: @transaction_params
-        
-        expect(response).to have_http_status(:ok)
-
-        transaction = Transaction.last
-        expect(transaction['market_name']).to eq('STOCK')
-        expect(transaction['market_order_type']).to eq('MARKET')
-        expect(transaction['transaction_type']).to eq('BUY')
-        expect(transaction['status']).to eq('FILLED')
-        expect(transaction['symbol']).to eq('AAPL')
-        expect(transaction['quantity']).to eq(100)
-        expect(transaction['price']).to eq(150.00)
-
-        portfolio = Portfolio.find(@portfolio.id)
-
-        expect(portfolio['buying_power']).to eq(850.00)
-        expect(portfolio['settled_cash']).to eq(850.00)
-        expect(portfolio['market_value']).to eq(650.00)
+    end
+    describe "POST /process_market_order_buy" do
+      context "when a trader successfully buys stock market order" do
+        it "should send a JSON response with a success message and transaction receipt and updated profile" do
+          post "/portfolios/#{@portfolio.id}/transactions/market_order/buy", params: @transaction_params
+          
+          expect(response).to have_http_status(:ok)
+  
+          transaction = Transaction.last
+          expect(transaction['market_name']).to eq('STOCK')
+          expect(transaction['market_order_type']).to eq('MARKET')
+          expect(transaction['transaction_type']).to eq('BUY')
+          expect(transaction['status']).to eq('FILLED')
+          expect(transaction['symbol']).to eq('AAPL')
+          expect(transaction['quantity']).to eq(100)
+          expect(transaction['price']).to eq(150.00)
+  
+          portfolio = Portfolio.find(@portfolio.id)
+  
+          expect(portfolio['buying_power']).to eq(850.00)
+          expect(portfolio['settled_cash']).to eq(850.00)
+          expect(portfolio['market_value']).to eq(650.00)
+        end
+      end
+      context "when a trader was unsuccessful in selling stock market order" do
+        it "should send a JSON response with a transaction failed message" do
+          @transaction_params[:transaction][:quantity] = 0
+          post "/portfolios/#{@portfolio.id}/transactions/market_order/sell", params: @transaction_params
+  
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
       end
     end
-    context "when a trader was unsuccessful in selling stock market order" do
-      it "should send a JSON response with a transaction failed message" do
-        @transaction_params[:transaction][:quantity] = 0
-        post "/portfolios/#{@portfolio.id}/transactions/market_order/sell", params: @transaction_params
-
-        expect(response).to have_http_status(:unprocessable_entity)
+  
+    describe "POST /process_market_order_sell" do
+      context "when a trader successfully sells stock market order" do
+        it "should send a JSON response with a success message and transaction receipt and updated profile" do
+          @transaction_params[:transaction][:transaction_type] = 'SELL'
+          post "/portfolios/#{@portfolio.id}/transactions/market_order/sell", params: @transaction_params
+          
+          expect(response).to have_http_status(:ok)
+  
+          transaction = Transaction.last
+          expect(transaction['market_name']).to eq('STOCK')
+          expect(transaction['market_order_type']).to eq('MARKET')
+          expect(transaction['transaction_type']).to eq('SELL')
+          expect(transaction['status']).to eq('FILLED')
+          expect(transaction['symbol']).to eq('AAPL')
+          expect(transaction['quantity']).to eq(100)
+          expect(transaction['price']).to eq(150.00)
+  
+          portfolio = Portfolio.find(@portfolio.id)
+  
+          expect(portfolio['buying_power']).to eq(1150.00)
+          expect(portfolio['settled_cash']).to eq(1150.00)
+          expect(portfolio['market_value']).to eq(350.00)
+        end
+      end
+      context "when a trader was unsuccessful in selling stock market order" do
+        it "should send a JSON response with a transaction failed message" do
+          @transaction_params[:transaction][:price] = -1500.00
+          post "/portfolios/#{@portfolio.id}/transactions/market_order/sell", params: @transaction_params
+  
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
       end
     end
   end
 
-  describe "POST /process_market_order_sell" do
-    context "when a trader successfully sells stock market order" do
-      it "should send a JSON response with a success message and transaction receipt and updated profile" do
-        @transaction_params[:transaction][:transaction_type] = 'SELL'
-        post "/portfolios/#{@portfolio.id}/transactions/market_order/sell", params: @transaction_params
-        
-        expect(response).to have_http_status(:ok)
-
-        transaction = Transaction.last
-        expect(transaction['market_name']).to eq('STOCK')
-        expect(transaction['market_order_type']).to eq('MARKET')
-        expect(transaction['transaction_type']).to eq('SELL')
-        expect(transaction['status']).to eq('FILLED')
-        expect(transaction['symbol']).to eq('AAPL')
-        expect(transaction['quantity']).to eq(100)
-        expect(transaction['price']).to eq(150.00)
-
-        portfolio = Portfolio.find(@portfolio.id)
-
-        expect(portfolio['buying_power']).to eq(1150.00)
-        expect(portfolio['settled_cash']).to eq(1150.00)
-        expect(portfolio['market_value']).to eq(350.00)
+  describe "Stock limit orders" do
+    before(:each) do
+      @transaction_params = { 
+        transaction: {
+          market_name: 'STOCK',
+          market_order_type: 'LIMIT',
+          transaction_type: 'BUY',
+          status: 'UNFULFILLED',
+          symbol: 'AAPL',
+          quantity: 100,
+          price: 150.00
+        }
+      }
+    end
+    describe "POST /process_limit_order_buy" do
+      context "when a trader successfully processed to buy stock limit order" do
+        it "should send a JSON response with a success message and transaction receipt and updated profile" do
+          post "/portfolios/#{@portfolio.id}/transactions/limit_order/buy", params: @transaction_params
+          
+          expect(response).to have_http_status(:ok)
+  
+          transaction = Transaction.last
+          expect(transaction['market_name']).to eq('STOCK')
+          expect(transaction['market_order_type']).to eq('LIMIT')
+          expect(transaction['transaction_type']).to eq('BUY')
+          expect(transaction['status']).to eq('UNFULFILLED')
+          expect(transaction['symbol']).to eq('AAPL')
+          expect(transaction['quantity']).to eq(100)
+          expect(transaction['price']).to eq(150.00)
+  
+          portfolio = Portfolio.find(@portfolio.id)
+  
+          expect(portfolio['buying_power']).to eq(850.00)
+          expect(portfolio['settled_cash']).to eq(850.00)
+          expect(portfolio['market_value']).to eq(500.00)
+        end
+      end
+      context "when a trader was unsuccessful to process buy stock limit order" do
+        it "should send a JSON response with a transaction failed message" do
+          @transaction_params[:transaction][:quantity] = 0
+          post "/portfolios/#{@portfolio.id}/transactions/market_order/sell", params: @transaction_params
+  
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
       end
     end
-    context "when a trader was unsuccessful in selling stock market order" do
-      it "should send a JSON response with a transaction failed message" do
-        @transaction_params[:transaction][:price] = -1500.00
-        post "/portfolios/#{@portfolio.id}/transactions/market_order/sell", params: @transaction_params
-
-        expect(response).to have_http_status(:unprocessable_entity)
-      end
+  
+    describe "POST /process_limit_order_sell" do
+  
     end
   end
 end
