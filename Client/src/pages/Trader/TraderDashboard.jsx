e75dbc989a89fd14f3eb9e93b471d8c59ae652ad
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-30 03:40:06 +0800

    wip: adding api call to get user's portfolio

--- Client/src/pages/Trader/TraderDashboard.jsx
+++ Client/src/pages/Trader/TraderDashboard.jsx
@@ -1,4 +1,24 @@
-export default function TraderDashboard(){
+import { useEffect } from "react"
+
+export default function TraderDashboard(props){
+    const { api } = props
+    useEffect(()=>{
+        getUserPortfolio()
+    },[])
+    const getUserPortfolio = async() => {
+        try {
+            const response = await api.get("signup", {
+                
+            })
+            if(response.status === 200) {
+                console.log(response.data)
+            } else {
+                console.log(response.status)
+            }
+        } catch(error) {
+            console.log(error)
+        }
+    }
     return(
         <div>
             <h1>TraderDashboard</h1>
