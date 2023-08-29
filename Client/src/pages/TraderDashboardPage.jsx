ba3e51445b887b2538a2f18557259c7c46d77464
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-29 13:44:53 +0800

    Feat: Added a new page

--- Client/src/pages/TraderDashboardPage.jsx
+++ Client/src/pages/TraderDashboardPage.jsx
@@ -2,9 +2,8 @@ import { Link } from "react-router-dom"
 
 export default function TraderDashboardPage(){
     return(
-        <>
-            <h1>TraderDashboardPage</h1>
-            <Link type="button" to="/">Logout</Link>
-        </>
+        <div>
+            
+        </div>
     )
 }
