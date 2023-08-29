0e50eb432668446ec247bc1bce89b6ba5140ce78
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-30 03:27:25 +0800

    feat: added basic HTML to trader dashboard view

--- Client/src/pages/Trader/TraderDashboard.jsx
+++ Client/src/pages/Trader/TraderDashboard.jsx
@@ -2,6 +2,24 @@ export default function TraderDashboard(){
     return(
         <div>
             <h1>TraderDashboard</h1>
+            <div>
+                <h1>Welcome back, !</h1>
+                <div>$</div>
+                <div>
+                    <div>
+                        <div>$</div>
+                        <div>SETTLED CASH</div>
+                    </div>
+                    <div>
+                        <div>$</div>
+                        <div>MARKET VALUE</div>
+                    </div>
+                    <div>
+                        <div>$</div>
+                        <div>BUYING POWER</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
