6d68541e9d49d319d19e8efae1715ca1b8137e2a
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-30 14:04:28 +0800

    refactor: added external CSS for custom button for hover effect; removed style for button

--- Client/src/components/TraderSideBar.jsx
+++ Client/src/components/TraderSideBar.jsx
@@ -1,24 +1,17 @@
-import { Button } from 'react-bootstrap'
+import "../components/TraderSideBar.css"
 
 export default function TraderSideBar(props){
     const { setChosenSidebarOption } = props
-    const sideBarButtonStyle = {
-        backgroundColor: "#093F5A",
-        fontSize: "2rem", 
-        padding: "0.25em 2em 0.25em 2em", 
-        color: "#4C95A9", 
-        border: "none"
-    }
     return(
-        <div className="d-flex flex-column" style={{backgroundColor: "#1F2039", width: "18%"}}>
+        <div className="d-flex flex-column h-100" style={{backgroundColor: "#1F2039", width: "414px"}}>
             <div className="d-flex align-items-center px-3 py-3 gap-3">
-                <img alt="logo" src={logo} width="15%" height="60%"/>
-                <h1>LEO TRADING</h1>
+                <img className="logo-2" alt="Logo" src="/src/assets/img/logo-1.png" />
+                <div className="text-wrapper-11">LEO TRADING</div>
             </div>
-            <div className="d-flex flex-column gap-3" style={{color: "#4C95A9"}}>
-                <Button style={sideBarButtonStyle} onClick={()=>setChosenSidebarOption("DASHBOARD")}>Dashboard</Button>
-                <Button style={sideBarButtonStyle} onClick={()=>setChosenSidebarOption("MARKET")}>Market</Button>
-                <Button style={sideBarButtonStyle} onClick={()=>setChosenSidebarOption("PORTFOLIO")}>Portfolio</Button>
+            <div className="d-flex flex-column gap-3" style={{color: "#4C95A9", marginTop: "30%"}}>
+                <button className="custom-button"  onClick={()=>setChosenSidebarOption("DASHBOARD")}>Dashboard</button>
+                <button className="custom-button"  onClick={()=>setChosenSidebarOption("MARKET")}>Market</button>
+                <button className="custom-button"  onClick={()=>setChosenSidebarOption("PORTFOLIO")}>Portfolio</button>
             </div>
         </div>
     )
