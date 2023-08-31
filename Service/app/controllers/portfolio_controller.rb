c4ac6e8c805e813ce22e780706893d7f0c248d7c
jamesS0804 <sucaldito.james0804@gmail.com>
2023-08-31 13:13:06 +0800

    feat: added route and controller action for 'GET /get_all_transactions'

--- Service/app/controllers/portfolio_controller.rb
+++ Service/app/controllers/portfolio_controller.rb
@@ -10,4 +10,14 @@ class PortfolioController < ApplicationController
             data: portfolio
         }, status: :ok
     end
+    def get_all_transactions
+        user = User.find(params[:user_id])
+        portfolio = user.portfolios.first
+        transactions = portfolio.transactions.all
+
+        render json: {
+            status: { code: 200, message: 'Transactions obtained' },
+            data: transactions
+        }, status: :ok
+    end
 end
