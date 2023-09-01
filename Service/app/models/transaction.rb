20561db46800a1663f77de95e945ace379c573c0
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-01 09:53:27 +0800

    fix: renamed typo from price to price_per_share

--- Service/app/models/transaction.rb
+++ Service/app/models/transaction.rb
@@ -6,11 +6,11 @@ class Transaction < ApplicationRecord
 
     enum transaction_type: [ :BUY, :SELL ]
 
-    validates :price, numericality: { greater_than_or_equal_to: 0, message: 'must be greater than or equal to 0' }
+    validates :price_per_share, numericality: { greater_than_or_equal_to: 0, message: 'must be greater than or equal to 0' }
     validates :quantity, numericality: {greater_than: 0, message: 'must be greater than 0'}
 
     private
-
+ 
     def calculate_amount
         self.amount = price_per_share * quantity
     end
