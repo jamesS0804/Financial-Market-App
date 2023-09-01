72a176d4fc684d4a92f72909246724c7728a7566
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-01 09:16:53 +0800

    feat: added new callback in Transaction model to calculate amount based from price_per_share and quantity; renamed price attribute to price_per_share as per recent update in Transactions table; added new amount attribute in Serializer

--- Service/app/models/transaction.rb
+++ Service/app/models/transaction.rb
@@ -2,8 +2,16 @@ class Transaction < ApplicationRecord
     belongs_to :portfolio
     belongs_to :market, foreign_key: 'market_name', primary_key: 'name'
 
+    before_save :calculate_amount
+
     enum transaction_type: [ :BUY, :SELL ]
 
     validates :price, numericality: { greater_than_or_equal_to: 0, message: 'must be greater than or equal to 0' }
     validates :quantity, numericality: {greater_than: 0, message: 'must be greater than 0'}
+
+    private
+
+    def calculate_amount
+        self.amount = price_per_share * quantity
+    end
 end
--- Service/app/serializers/transaction_serializer.rb
+++ Service/app/serializers/transaction_serializer.rb
@@ -1,6 +1,6 @@
 class TransactionSerializer
   include JSONAPI::Serializer
-  attributes :id, :trader, :market_name, :market_order_type, :transaction_type, :status, :quantity, :price_per_share, :created_at, :updated_at
+  attributes :id, :trader, :market_name, :market_order_type, :transaction_type, :status, :quantity, :price_per_share, :created_at, :updated_at, :amount
 
   attributes :trader do |object|
     object.portfolio.user.email
