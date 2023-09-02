364d6b23280c187eca0a4c5260a1b783926e4c6a
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-03 04:32:20 +0800

    feat: added controller validation to precent pending users from doing transactions

--- Service/app/controllers/transactions_controller.rb
+++ Service/app/controllers/transactions_controller.rb
@@ -1,5 +1,6 @@
 class TransactionsController < ApplicationController
     before_action :authenticate_user!
+    before_action :check_if_pending
     
     def process_market_order_buy
         portfolio = Portfolio.find(params[:portfolio_id])
@@ -92,6 +93,18 @@ class TransactionsController < ApplicationController
     end
 
     private
+
+    def check_if_pending
+        unless current_user.signup_status('APPROVED')
+            render json: {
+                status: {
+                    status: { 
+                        code: 403, 
+                        message: 'Transaction feature is locked. You will receive an email once approved.' }
+                }, status: :forbidden
+            }
+        end
+    end
 
     def transaction_params
         params.require(:transaction).permit(:market_name, :symbol, :price_per_share, :amount, :market_order_type, :status, :quantity, :transaction_type)
