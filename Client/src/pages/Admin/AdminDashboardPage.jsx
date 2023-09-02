b52225bf5594a936c05a93ba0a61b7760a488c57
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-02 08:20:21 +0800

    maint: prop drilled necessary props for child component use

--- Client/src/pages/Admin/AdminDashboardPage.jsx
+++ Client/src/pages/Admin/AdminDashboardPage.jsx
@@ -1,10 +1,36 @@
-import { Link } from "react-router-dom"
+import AdminSidebar from "../../components/Admin/AdminSidebar";
+import Navbar from "../../components/Navbar";
+import { useState } from "react";
+import AdminAllTraders from "../../components/Admin/AdminAllTraders"
+import AdminAllTransactions from "../../components/Admin/AdminAllTransactions";
 
-export default function AdminDashboardPage(){
-    return(
-        <>
-            <h1>AdminDashboardPage</h1>
-            <Link type="button" to="/">Logout</Link>
-        </>
-    )
-}
+export default function Admin (props) {
+  const { setIsLoading, setAuthAlert, renderAlertVariant, authAlert, isLoading } = props
+  const [ chosenSidebarOption, setChosenSidebarOption ] = useState()
+  const renderMainTraderView = () => {
+      switch(chosenSidebarOption){
+          case "TRANSACTIONS":
+              return <AdminAllTransactions
+                      />
+          default:
+              return <AdminAllTraders
+                          setIsLoading={setIsLoading}
+                          setAuthAlert={setAuthAlert}
+                          renderAlertVariant={renderAlertVariant}
+                          authAlert={authAlert}
+                          isLoading={isLoading}
+                      />
+      }
+    }
+  return (
+    <div className="d-flex" style={{width: "100dvw", height: "100dvh"}}>
+      <AdminSidebar setChosenSidebarOption={setChosenSidebarOption}/>
+      <section className="flex-grow-1 d-flex flex-column" style={{backgroundColor: "#83D9E3"}}>
+        <Navbar/>
+        <main className="flex-grow-1">
+            { renderMainTraderView() }
+        </main>
+      </section>
+    </div>
+  );
+};
