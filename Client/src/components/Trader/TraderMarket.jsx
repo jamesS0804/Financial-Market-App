0a2ecb737a8c5a47ea7ffab124a7e2fd1029c9a7
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-03 14:00:53 +0800

    maint: updated HTML and css for TraderMarket

--- Client/src/components/Trader/TraderMarket.jsx
+++ Client/src/components/Trader/TraderMarket.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import authenticated_api from "../../utils/authenticated_api";
 import { Button } from "react-bootstrap";
 import StockModal from "./StockModal";
+import logo from "../../assets/img/logo-1.png"
+import "./TraderMarket.css"
 
 export default function TraderMarket(props){
     const { currentUserPortfolio } = props
@@ -49,15 +51,15 @@ export default function TraderMarket(props){
                 setMarketStockData={setMarketStockData}
                 currentUserPortfolio={currentUserPortfolio}
             />
-            <h1 className="px-3">Market</h1>
+            <h1 style={{fontWeight: "bold", fontSize: "2.5rem", padding: "0 1em 0 0.80em"}}>Stock Market</h1>
             <table className="container-fluid" style={{borderCollapse: "collapse"}}>
-                <thead className="sticky-header" style={{borderBottom: "1px solid black",}}>
-                    <tr>
-                        <th id="market-name" style={{width: "30%", padding: "0.5em 1em"}}>Market</th>
-                        <th id="market-change">Change 1D</th>
-                        <th id="market-buy">Buy</th>
-                        <th id="market-sell">Sell</th>
-                        <th id="market-average-daily-volume">Average Daily Volume</th>
+                <thead className="sticky-header">
+                    <tr style={{fontSize: "1.5rem", fontWeight: "bold", borderBottom: "2px solid black"}}>
+                        <th style={{width: "25%", padding: "0 0 0 1.5em"}} id="market-name">Market</th>
+                        <th className="p-3 text-center" id="market-change">Change (1D)</th>
+                        <th className="p-3 text-center" id="market-buy">Buy</th>
+                        <th className="p-3 text-center" id="market-sell">Sell</th>
+                        <th className="p-3 text-center" id="market-average-daily-volume">Average Daily Volume</th>
                     </tr>
                 </thead>
                 <tbody style={{overflowY: "auto"}}>
@@ -65,52 +67,55 @@ export default function TraderMarket(props){
                     {
                         stockData.map((stock)=>{
                             return (
-                                <tr key={stock.id} style={{border: "1px solid black", borderWidth: "1px 0 1px 0"}}>
-                                <td headers="market-name" style={{width: "30%", padding: "0.5em 1em"}}>
-                                    <div className="d-flex flex-column">
-                                        <span>{stock.symbol}</span>
-                                        <span>{stock.name}</span>
-                                    </div>
-                                </td>
-                                <td headers="market-change">
-                                    <div className="d-flex flex-column">
-                                        <span style={
-                                            stock.change > 0 ? {color: "green"} : {color: "red"}
-                                        }>{parseFloat(stock.change).toFixed(2)}</span>
-                                        <span style={
-                                            stock.change > 0 ? {color: "green"} : {color: "red"}
-                                        }>({stock.change_percent})%</span>
-                                    </div>
-                                </td>
-                                <td headers="market-buy" >
-                                    <Button onClick={()=>openStockModal(stock.symbol, stock.price, "BUY", stock.change, stock.change_percent)} style={{position: "relative", width: "55%", paddingLeft: "2em"}}>
-                                        <span style={{
-                                            position: "absolute", left: 0, top: 0, height: "100%",
-                                            display: "flex", justifyContent: "center", alignItems: "center",
-                                            borderRadius: "0.3em 0 0 0.3em", backgroundColor: "#005E69",
-                                            padding: "0.5em"
-                                        }}>
-                                            <span>B</span>
-                                        </span>
-                                        <span>{stock.price}</span>
-                                    </Button>
-                                </td>
-                                <td headers="market-sell">
-                                    <Button onClick={()=>openStockModal(stock.symbol, stock.price, "SELL", stock.change, stock.change_percent)} style={{position: "relative", width: "55%", paddingLeft: "2em"}}>
-                                        <span style={{
-                                            position: "absolute", left: 0, top: 0, height: "100%",
-                                            display: "flex", justifyContent: "center", alignItems: "center",
-                                            borderRadius: "0.3em 0 0 0.3em", backgroundColor: "#005E69",
-                                            padding: "0.5em"
-                                        }}>
-                                            <span>S</span>
-                                        </span>
-                                        <span>{stock.price}</span>
-                                    </Button>
-                                </td>
-                                <td headers="market-average-daily-volume">
-                                    <span>{stock.average_daily_volume}</span>
-                                </td>
+                                <tr key={stock.id} style={{border: "1px solid black", borderWidth: "0 0 1px 0"}}>
+                                    <td headers="market-name" className="d-flex gap-3 align-items-center h-100" style={{padding: "0", margin: "1rem 0rem 0rem 2rem"}}>
+                                        <img style={{border: "2px solid black", backgroundColor: "black", borderRadius: "0.5rem", padding: "0.2em"}} alt="brand-logo-placeholder" src={logo} width="50px" height="50px"/>
+                                        <div className="d-flex flex-column">
+                                            <span style={{fontWeight: "bold", fontSize: "1.35rem"}}>{stock.symbol}</span>
+                                            <span style={{fontSize: "1.1rem"}}>{stock.company_name}</span>
+                                        </div>
+                                    </td>
+                                    <td headers="market-change" style={{padding: "0", margin: "0"}}>
+                                        <div className="d-flex flex-column justify-content-center align-items-center">
+                                            <span style={
+                                                stock.change > 0 ? {color: "green", fontSize: "1.5rem", fontWeight: "bold"} : {color: "#A73121", fontSize: "1.5rem", fontWeight: "bold"}
+                                            }>{parseFloat(stock.change_percent).toFixed(2)}%</span>
+                                            <span style={
+                                                stock.change > 0 ? {color: "green"} : {color: "#A73121"}
+                                            }>({parseFloat(stock.change).toFixed(4)})</span>
+                                        </div>
+                                    </td>
+                                    <td headers="market-buy" style={{width: "12%"}}>
+                                        <Button onClick={()=>openStockModal(stock.symbol, stock.price, "BUY", stock.change, stock.change_percent)} 
+                                            style={{position: "relative", width: "100%", paddingLeft: "2em", border: "none"}}>
+                                            <span style={{
+                                                position: "absolute", left: 0, top: 0, height: "100%",
+                                                display: "flex", justifyContent: "center", alignItems: "center",
+                                                borderRadius: "0.3em 0 0 0.3em", backgroundColor: "#005E69",
+                                                padding: "0.5em"
+                                            }}>
+                                                <span>B</span>
+                                            </span>
+                                            <span>{stock.price}</span>
+                                        </Button>
+                                    </td>
+                                    <td headers="market-sell" style={{width: "12%"}}>
+                                        <Button onClick={()=>openStockModal(stock.symbol, stock.price, "SELL", stock.change, stock.change_percent)} 
+                                            style={{position: "relative", width: "100%", paddingLeft: "2em", border: "none"}}>
+                                            <span style={{
+                                                position: "absolute", left: 0, top: 0, height: "100%",
+                                                display: "flex", justifyContent: "center", alignItems: "center",
+                                                borderRadius: "0.3em 0 0 0.3em", backgroundColor: "#005E69",
+                                                padding: "0.5em"
+                                            }}>
+                                                <span>S</span>
+                                            </span>
+                                            <span>{stock.price}</span>
+                                        </Button>
+                                    </td>
+                                    <td headers="market-average-daily-volume" className="d-flex justify-content-center align-items-center">
+                                        <span>{stock.average_daily_volume}</span>
+                                    </td>
                                 </tr>
                             )
                         })
