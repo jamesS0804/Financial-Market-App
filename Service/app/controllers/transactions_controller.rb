234afe30177f751027a746d3175db1968fb0438f
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-04 07:09:16 +0800

    refactor: refactored handle_market_order private method and modularized it to lessen redundant code and prevent too much if else

--- Service/app/controllers/transactions_controller.rb
+++ Service/app/controllers/transactions_controller.rb
@@ -15,27 +15,16 @@ class TransactionsController < ApplicationController
     def handle_market_order(transaction_type)
         portfolio = Portfolio.find(params[:portfolio_id])
 
-        quantity = transaction_params[:quantity].to_f
-        price_per_share = transaction_params[:price_per_share].to_f
-
-        if portfolio.has_enough_balance?(price_per_share, quantity)
-            transaction = portfolio.transactions.new(transaction_params)
-            symbol = transaction_params[:symbol]
-
-            if transaction.save
-                transaction_type.to_sym == :BUY ? portfolio.buy_unit(symbol, quantity, price_per_share) : portfolio.sell_unit(symbol, quantity, price_per_share)
-                
-                render_json_response(
-                    data: { 
-                        transaction_receipt: TransactionSerializer.new(transaction).serializable_hash[:data][:attributes],
-                        portfolio: portfolio
-                    }, 
-                    message: 'Transaction processed')
-            else
-                render_json_response(status_code: 422 , message: "Transaction failed")
+        if transaction_type.to_sym == :BUY 
+            if !portfolio.has_enough_balance?(transaction_params[:price_per_share], transaction_params[:quantity])
+                render_json_response(status_code: 422 , message: "Transaction failed. You don't have enough balance") and return
             end
+            process_transaction_save(:BUY, portfolio, transaction_params) 
         else
-            render_json_response(status_code: 422 , message: "Transaction failed. You don't have enough balance")
+            if !portfolio.has_enough_unit?(transaction_params[:symbol], transaction_params[:quantity])
+                render_json_response(status_code: 422 , message: "Transaction failed. Not enough unit quantity.") and return
+            end
+            process_transaction_save(:SELL, portfolio, transaction_params) 
         end
     end
 
@@ -43,6 +32,26 @@ class TransactionsController < ApplicationController
         if current_user.signup_status == 'PENDING'
             render_json_response(status_code: 403 , message: 'Transaction feature is locked. You will receive an email once approved.')
         end
+    end
+
+    def process_transaction_save(transaction_type, portfolio, transaction_params)
+        transaction = portfolio.transactions.create(transaction_params)
+        
+        if !transaction.save
+            render_json_response(status_code: 422 , message: "Transaction failed.") and return
+        end
+
+        symbol = transaction_params[:symbol]
+        quantity = transaction_params[:quantity].to_f
+        price_per_share = transaction_params[:price_per_share].to_f
+
+        transaction_type.to_sym == :BUY ? portfolio.buy_unit(symbol, quantity, price_per_share) : portfolio.sell_unit(symbol, quantity, price_per_share)
+        render_json_response(
+                    data: { 
+                        transaction_receipt: TransactionSerializer.new(transaction).serializable_hash[:data][:attributes],
+                        portfolio: portfolio
+                    }, 
+                    message: 'Transaction processed.')
     end
 
     def render_json_response(status_code: nil, message: , data: nil)
