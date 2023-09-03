b1695707103f4155671e00fd569db8176752e2bd
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-03 09:33:43 +0800

    maint: setup confirmatiosn controller

--- Service/config/application.rb
+++ Service/config/application.rb
@@ -36,7 +36,7 @@ module Service
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    # config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
