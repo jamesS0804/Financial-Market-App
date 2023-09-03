7acbf95dd2f293c1487093f6306079b6b5885ef6
jamesS0804 <sucaldito.james0804@gmail.com>
2023-09-03 18:52:23 +0800

    maint: updated test cases for model portfolio_unit; for controllers admins_controller, transactions_controller maint: updated logic for controller actions in admins_controller, transactions_controller

--- Service/spec/models/portfolio_unit_spec.rb
+++ Service/spec/models/portfolio_unit_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe PortfolioUnit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:symbol) }
+    it { should validate_presence_of(:company_name) }
+    it { should validate_presence_of(:price_per_share) }
+
+    it 'validates that symbol is case-sensitively unique' do
+      user = User.create(email: 'user1@example.com', password: 'password', first_name: 'test', last_name: 'test', role: "TRADER", signup_status: 'APPROVED', confirmed_at: Date.today)
+      portfolio = user.portfolios.first
+      unit1 = portfolio.portfolio_units.create(
+        symbol: 'AAPL',
+        company_name: 'Apple Inc.',
+        price_per_share: 150,
+        quantity: 2,
+        amount: 500
+      )
+      unit2 = portfolio.portfolio_units.create(
+        symbol: 'AAPL',
+        company_name: 'Apple Inc.',
+        price_per_share: 150,
+        quantity: 2,
+        amount: 500
+      )
+      expect(unit1).to be_valid
+      expect(unit2).not_to be_valid
+      expect(unit2.errors[:symbol]).to include('has already been taken')
+      expect(unit2.errors[:company_name]).to include('has already been taken')
+    end
+
+    it 'validates that quantity is greater than or equal to 0' do
+      should validate_numericality_of(:quantity).is_greater_than_or_equal_to(0).with_message('must be greater than 0')
+    end
+
+    it 'validates that amount is greater than or equal to 0' do
+      should validate_numericality_of(:amount).is_greater_than_or_equal_to(0).with_message('must be greater than 0')
+    end
+  end
+
+  describe 'associations' do
+    it { should belong_to(:portfolio) }
+  end
 end
